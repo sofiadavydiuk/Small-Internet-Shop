e1bb49915f83628f891c76e4e4e39ab27d0627df
sofiadavydiuk <sofiadavydyuk@gmail.com>
2018-03-07 10:24:54 +0100

    Done: -changed Date for Smartphones

--- Task - Level1/src/js/components/Products.js
+++ Task - Level1/src/js/components/Products.js
@@ -3,11 +3,12 @@ import React from "react";
 export default class Article extends React.Component {
   render() {
     const { title } = this.props;
+    const { price } = this.props;
 
     return (
       <div class="col-md-4">
         <h4>{title}</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+        <p>{price}</p>
         <a class="btn btn-default" href="#">More Info</a>
       </div>
     );
--- Task - Level1/src/js/pages/Smartphones.js
+++ Task - Level1/src/js/pages/Smartphones.js
@@ -1,38 +1,21 @@
 import React from "react";
 
-import Article from "../components/Article";
+import Article from "../components/Products";
 
 export default class Featured extends React.Component {
     render() {
         const Articles = [
-            "Some Article",
-            "Some Other Article",
-            "Yet Another Article",
-            "Still More",
-            "Some Article",
-            "Some Other Article",
-            "Yet Another Article",
-            "Still More",
-            "Some Article",
-            "Some Other Article",
-            "Yet Another Article",
-            "Still More",
-        ].map((title, i) => <Article key={i} title={title}/>);
+            ["Iphone 6", "100zl"],
+            ["Iphone 6", "100zl"],
+            ["Iphone 6", "100zl"],
+            ["Iphone 6", "100zl"]
+        ].map((title, price, i) => <Article key={i} title={title} price={price}/>);
 
-        const adText = [
-            "Ad spot #1",
-            "Ad spot #2",
-            "Ad spot #3",
-            "Ad spot #4",
-            "Ad spot #5",
-        ];
-
-        console.log("featured");
         return (
             <div>
                 <h1>Smartphones</h1>
 
-                <div class="row">{Articles}</div>
+                <div class="row" >{Articles}</div>
             </div>
         );
     }
