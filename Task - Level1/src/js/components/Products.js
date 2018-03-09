8b07df2b2ae34b1c72200132bc1d59a8892e27d3
sofiadavydiuk <sofiadavydyuk@gmail.com>
2018-03-09 14:20:15 +0100

    Done: - pages with date - http requests in react (in progress)

--- Task - Level1/src/js/components/Products.js
+++ Task - Level1/src/js/components/Products.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 export default class Products extends React.Component {
-  render() {
-    let attr = this.props;
+	render() {
+		let name  = this.props.params.name;
+		let price = this.props.params.price;
+		let logo  = this.props.params.logo;
 
-    console.log(attr);
-
-    return (
-      <div class="col-md-4">
-        <h4>{this.props.params.name}</h4>
-        <p>{this.props.params.price}</p>
-        <a class="btn btn-default" href="#">More Info</a>
-      </div>
-    );
-  }
+		return (
+			<div class="col-md-4">
+				<div className="product">
+					<img src={logo} alt="product" class="product__logo"/>
+					<h4 class="product__name">{name}</h4>
+					<p class="product__price">{price}</p>
+					<a class="btn btn-success product__btn" href="#">More Info</a>
+				</div>
+			</div>
+		);
+	}
 }
